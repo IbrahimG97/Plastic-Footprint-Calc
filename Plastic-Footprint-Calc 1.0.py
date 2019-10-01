02b8f94b84c64e0ced2885f07eecc2dd9aed473d
IbrahimG97 <00291879@student.necc.edu>
2019-10-01 14:13:28 -0400

    Inital program file

---
+++ Plastic-Footprint-Calc 1.0.py
@@ -0,0 +1,19 @@
+#.... This program is designed to input various plastic items such as plastic straws, cups and bags and how much you use
+# of each item. For example I used 15 straws this week and I created this many pounds of plastic waste for the week.
+# the goal is to have the program be able to calculate multiple items at once instead of one at a time.
+# for example it will tell you your combined total of plastic waste based on how many straws bags and cups used etc.
+# The program will take that data and compare it to the national average in the USA
+# The Program will also take monetary savings into account if you curretly visit a place that offers it
+# Starbucks and whole foods are one of the places that offer a discount based on using reusable items
+# If the user does visit one of theses places then a monenetary savings for the week month and year will be displayed.
+
+#Start of project
+# Varibles are cups straws and bags
+
+plastic_items = ['straws','cups','bags']
+reusable_items = ['straws','cups','bags']
+
+print('Welcome to the plastic footprint calc')
+print()
+print('This program is designed to show you how many pounds of plastic you can save yearly, monthly and weekly.')
+print('This program can also show any monetary and planetary savings based on reusable choices.')
