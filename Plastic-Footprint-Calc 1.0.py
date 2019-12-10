03320973335b99a47cae8fc7c0f1f404c960666e
Ibrahim Gunsever <ibrahimgunsever97@gmail.com>
2019-12-10 01:51:45 -0500

    > This version is the final presentation version for my class >I am hoping to use this program as practic eto improve my python skills,

--- Plastic-Footprint-Calc 1.0.py
+++ Plastic-Footprint-Calc 1.0.py
@@ -12,7 +12,7 @@ print('Welcome to the Plastic Consumption Calc')
 print('This program is designed to show you how many Grams of plastic you consume Yearly, Monthly, Weekly')
 print('This program can also show any monetary and planetary savings based on reusable choices.')
 print('Lets start by finding out your plastic usage')
-
+boolean=False
 
 def plastic_straws():     #Function used to run straw consumption in grams.
     numofstraws = input('Enter number of Straws used this week.\n')
@@ -41,22 +41,25 @@ def starbucks_savings(): # this is a function used to calculate the monetary sav
     numofcups = float(numofcups)
     savings = float(0.10)
     savings = float(savings)
+    print('Starbucks offers 10 cents off any order if you use a reusable mug!')
     print('By switching to a reusable cup you could save,','In one week',numofcups*savings, 'Cents', 'In one month', numofcups*savings*4,'Cents','In one year', numofcups*savings*52, 'Cents')
+
 def wholefoods_savings(): # this is a function used to calculate the monetary savings of using a reusable bag at whole foods for a week, year, and month
     numofbags = input('Enter the number of bags used this week\n')
     numofbags = float(numofbags)
     savings = float(0.05)
     savings = float(savings)
+    print('Wholefoods offers 5 cents off any purchase with use of a reusable bag!')
     print('By switching to a reusable bag you could save,','In one week',numofbags*savings, 'Cents', 'In one month', numofbags*savings*4, 'Cents', 'In one year', numofbags*savings*52, 'Cents')
 
 def choosesavings(): #this function allows you to choose between monetary savings for starbucks or wholefoods
-    if input() == 'Starbucks':
-        starbucks_savings()
-    elif input() == 'Wholefoods':
-        wholefoods_savings()
-    else:
-        print('Please select either Starbucks or Wholefoods, Answer is case sensitive!')
-        choosesavings()
+        if input() == 'Starbucks':
+            starbucks_savings()
+        elif input() == 'Wholefoods':
+             wholefoods_savings()
+        else:
+            print('Please select either Starbucks or Wholefoods, Answer is case sensitive!')
+            choosesavings()
 
 
 
@@ -64,17 +67,15 @@ def choosesavings(): #this function allows you to choose between monetary saving
 
 #need to work on being able to calculate exact decimal values and possibly converting cents to dollar amounts.
 #See if its possible to have a currency placeholder.
-National_average = ['Insert calc for national average'] # add this at the end of the straw, bags and cup function instead as a single one.
 
-#put for or while loop to have the program select a function
 
-#print('Places like Starbucks and Wholefoods provide discounts if you use a reusable bag or mug,', '\n''Do you use Starbucks or Wholefoods?', 'Please awnser with either Starbucks or Wholefoods.')
-#
-#choosesavings()
+
+
+
 def maincalc():  # This is what allows the user to select which calculation they would like to do.
     boolean = False
 
-    while boolean == False:
+    while boolean == False: #For some reason If I used a regular while loop without boolean it would only consider the first option and nothing else. This method I got from Stack overflow
         item = input('Please select an item by typing its name. You can select straws, cups or bags.\n')
         if 'straws' in item:
             plastic_straws()
@@ -91,5 +92,26 @@ def maincalc():  # This is what allows the user to select which calculation they
 
 maincalc()
 print('The second part of this calculator allows you to see how much money you would save if you used reusable bags or cups at specific locations that offer discounts')
+print('Starbucks and Wholefoods offer discounts for using a reusable bag or cup. Please type in Starbucks or Wholefoods to being the 2nd part of the calculator.')
 choosesavings()
+
+print('Thank you for using the Plastic Foot Print calculator. The goal is to inspire users to use less plastic waste and use more reusable items!')
+
+
+
+
+
+
+
+
+#i = False Tried to fix the loop for wholefoods
+# while i == False:
+    #if input() == 'Starbucks':
+        #starbucks_savings()
+        #i = True
+    # if input() == 'Wholefoods':
+    #     wholefoods_savings()
+    #     i = True
+    # if i == False:
+    #     print('Please type either Starbucks or Wholefoods. Answer is case sensitive!')
 
